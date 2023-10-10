6072aabcd5b3a58bcdc6d90abc16669945d573c2
ecomeel <ecomeel@gmail.com>
2023-10-10 19:17:46 +0300

    model

--- src/scripts/controller.js
+++ src/scripts/controller.js
@@ -1,19 +1,66 @@
 import View from "./view";
 import Model from "./model";
 
+import { v4 as uuidv4 } from "uuid";
+
+const itemsFrSt = [
+    {
+        id: uuidv4(),
+        name: "Apple watch",
+        model: "Series 5 SE",
+        price: 529,
+        imgSrc: "../../img/products/apple-watch.png",
+        shortDesc:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, ratione?",
+        mainDesc:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi temporibus magnam dolorem nobis repellendus alias ab aspernatur error illum dolores.",
+        fullDesc:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat eius repellendus, omnis dolorum dolor et est natus quae tempora quidem labore sequi possimus quia ex odit cupiditate, excepturi provident reiciendis exercitationem eos. Quidem sunt quibusdam eius, qui autem soluta magni deserunt corrupti fugit velit architecto nam et magnam ut, officia nemo labore voluptatibus est consequatur quaerat tempore placeat, provident veritatis! Debitis aspernatur voluptatibus sapiente sed nisi dolorum eius enim at officia similique ipsa, laboriosam pariatur aliquam eum dolore corporis quas temporibus, deserunt fugit non atque, necessitatibus illo. Cum laborum sed ullam, ab consequatur natus nemo perspiciatis odit porro quisquam consectetur!",
+        rating: 4,
+    },
+    {
+        id: uuidv4(),
+        name: "Apple watch",
+        model: "Series 5 SE",
+        price: 529,
+        imgSrc: "../../img/products/apple-watch.png",
+        shortDesc:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, ratione?",
+        mainDesc:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi temporibus magnam dolorem nobis repellendus alias ab aspernatur error illum dolores.",
+        fullDesc:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat eius repellendus, omnis dolorum dolor et est natus quae tempora quidem labore sequi possimus quia ex odit cupiditate, excepturi provident reiciendis exercitationem eos. Quidem sunt quibusdam eius, qui autem soluta magni deserunt corrupti fugit velit architecto nam et magnam ut, officia nemo labore voluptatibus est consequatur quaerat tempore placeat, provident veritatis! Debitis aspernatur voluptatibus sapiente sed nisi dolorum eius enim at officia similique ipsa, laboriosam pariatur aliquam eum dolore corporis quas temporibus, deserunt fugit non atque, necessitatibus illo. Cum laborum sed ullam, ab consequatur natus nemo perspiciatis odit porro quisquam consectetur!",
+        rating: 4,
+    },
+    {
+        id: uuidv4(),
+        name: "Apple watch",
+        model: "Series 5 SE",
+        price: 529,
+        imgSrc: "../../img/products/apple-watch.png",
+        shortDesc:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, ratione?",
+        mainDesc:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi temporibus magnam dolorem nobis repellendus alias ab aspernatur error illum dolores.",
+        fullDesc:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat eius repellendus, omnis dolorum dolor et est natus quae tempora quidem labore sequi possimus quia ex odit cupiditate, excepturi provident reiciendis exercitationem eos. Quidem sunt quibusdam eius, qui autem soluta magni deserunt corrupti fugit velit architecto nam et magnam ut, officia nemo labore voluptatibus est consequatur quaerat tempore placeat, provident veritatis! Debitis aspernatur voluptatibus sapiente sed nisi dolorum eius enim at officia similique ipsa, laboriosam pariatur aliquam eum dolore corporis quas temporibus, deserunt fugit non atque, necessitatibus illo. Cum laborum sed ullam, ab consequatur natus nemo perspiciatis odit porro quisquam consectetur!",
+        rating: 4,
+    },
+];
+
 export default class Controller {
     constructor() {
         this.view = new View();
         this.model = new Model();
-
     }
 
     init() {
-        this.model.setIDsList()
-        console.log(this.model.getItemsIDs())
-
-        this.model.setItemsList()
-        console.log(this.model.getItems())
+        setTimeout(() => {
+            const arr = itemsFrSt;
+            this.model.setItemsList(arr); // set items list
+            this.model.setIDsList(); // set IDs 
+            this.view.renderItems(this.model.getItems()); // render items
+        }, 5000);
+      
     }
-
 }
--- src/scripts/view.js
+++ src/scripts/view.js
@@ -1,6 +1,32 @@
 export default class View {
     constructor() {
-        const itemsListNode = document.getElementById('itemsList');
+        this.itemsListNode = document.getElementById("itemsList");
+    }
 
+    renderItems(items) {
+        let itemsListHTML = "";
+        items.forEach((item) => {
+            itemsListHTML += `
+            <li class="preview-item">
+                <img
+                    class="preview-item__img"
+                    src=${item.imgSrc}
+                />
+                <h3 class="preview-item__name">${item.name}</h3>
+                <p class="preview-item__model">${item.model}</p>
+                <div class="to-bag-wrapper">
+                    <p class="preview-item__price">${item.price}$</p>
+                    <button
+                        class="preview-item__to-bag-btn black-btn"
+                    >
+                        <img
+                            src="img/buttons/to-bag-mini.png"
+                            alt=""
+                        />
+                    </button>
+                </div>
+            </li>`;
+        });
+        this.itemsListNode.innerHTML = itemsListHTML;
     }
 }
